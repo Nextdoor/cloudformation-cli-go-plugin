42372008f5874277973ef1c4b403475632f7c974
Angie Palm <angie@nextdoor.com>
2020-04-03 14:54:23 -0700

    Test

--- cfn/encoding/unmarshal.go
+++ cfn/encoding/unmarshal.go
@@ -3,22 +3,19 @@ package encoding
 import (
 	"encoding/json"
 	"log"
-	"strconv"
 )
 
 // Unmarshal converts stringified-JSON into the passed-in type
 func Unmarshal(data []byte, v interface{}) error {
 	var dataMap map[string]interface{}
 	log.Printf("\nUnmarshal: byte data: %+v\n", data)
-	log.Printf("\nUnmarshal: string data: %+v\n", string(data))
 	log.Printf("\nUnmarshal: dataMap: %+v\n", dataMap)
 
-	dataStr, err := strconv.Unquote(string(data))
-	if err != nil {
-		log.Printf("Unmarshal: err Unquoting")
-	}
-	log.Printf("\nUnmarshal: dataStr %s\n", dataStr)
-	err = json.Unmarshal([]byte(dataStr), &dataMap)
+	//dataStr, err := strconv.Unquote(string(data))
+	//if err != nil {
+	//	log.Printf("Unmarshal: err Unquoting")
+	//}
+	err := json.Unmarshal(data, &dataMap)
 	if err != nil {
 		return err
 	}
